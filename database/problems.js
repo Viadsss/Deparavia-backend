ead2075c60b34c9eeca4c31a07ce889a03fe453a
John Paul <113358424+Viadsss@users.noreply.github.com>
2024-07-07 21:02:15 +0800

    Add Templating Comments

--- database/problems.js
+++ database/problems.js
@@ -1,6 +1,12 @@
 import pool from "./database.js";
 
+// Simple
 // Problem 1
+// Problem 2
+// Problem 3
+
+// Medium
+// Problem 4
 // Display the marital statuses of patients where there are more than 5 patients in each status.
 export async function getProblem4() {
   const [rows] = await pool.query(`
@@ -12,3 +18,12 @@ export async function getProblem4() {
 
   return rows;
 }
+
+// Problem 5
+// Problem 6
+// Problem 7
+
+// Difficult
+// Problem 8
+// Problem 9
+// Problem 10
